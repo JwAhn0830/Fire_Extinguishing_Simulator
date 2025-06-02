10cb69b1c90aec35a1ad2a52fd537b1cadeb7bba
Jaewon_Ahn <jaewonahn1234@gmail.com>
2025-06-02 20:05:34 +0900

    Update : LED library

---
+++ sensors/Led/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "led.h"
+
+int main(int argc, char** argv) {
+	
+    int fd = ledLibInit();
+    
+    if (fd < 0) {
+        printf("failed open file\n");
+        return 1;
+    }
+    printf("hihihihih\n");
+    while(1) {
+        for (int i = 0; i<=7; i++) {
+            ledOnOff(i,1);
+            printf("LED %d ON\n", i + 1);
+            sleep(1);
+        }
+
+        for (int i = 0; i<=7; i++) {
+            ledOnOff(i,0);
+            printf("LED %d OFF\n", i + 1);
+            sleep(1);
+        }
+        printf("bye \n");
+        ledLibExit();
+        return 0;
+    }
+}
